6e9159798e09cae47050243a6523b999fae5053e
Rodrigo Lobo <correotaller278@gmail.com>
2024-12-03 11:48:03 -0300

    Sidebar - LoginPage

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -20,55 +20,58 @@ const Sidebar = () => {
 
       {/* Sidebar animado */}
       <motion.div
-        className="fixed top-0 left-0 h-full bg-gray-900 text-gray-400 w-44 p-2 md:w-64 md:p-4 shadow-lg z-40"
+        className="fixed top-0 left-0 h-full bg-gray-900 text-gray-400 w-44 p-2 md:w-64 md:p-4 shadow-lg z-40 flex flex-col justify-between"
         initial={{ x: '-100%' }}
         animate={{ x: isOpen ? 0 : '-100%' }}
         transition={{ duration: 0.5 }}
       >
-        {/* Título */}
-        <h2 className="md:text-lg text-cyber-blue font-bold uppercase my-2 md:my-4">Contacto</h2>
+        {/* CONTACTO */}
+        <div>
+          {/* Título */}
+          <h2 className="md:text-lg text-cyber-blue font-bold uppercase my-2 md:my-4">Contacto</h2>
 
-        {/* Información de contacto */}
-        <ul className="space-y-4">
-          <li className="flex items-center">
-            <FaMapMarkerAlt className="text-cyber-blue mr-1 md:mr-3" />
-            <span className=' text-xs md:text-base'>Saavedra 278, Bº Oeste<br />Juan B. Alberdi, Tucumán</span>
-          </li>
-          <li className="flex items-center">
-            <FaEnvelope className="text-cyber-blue mr-1 md:mr-3" />
-            <span className=' text-xs md:text-base'>correotaller278@gmail.com</span>
-          </li>
-          <li className="flex items-center">
-            <FaPhone className="text-cyber-blue mr-1 md:mr-3" />
-            <a href="tel:3865627955" className="hover:text-white transition text-xs md:text-base">
-              3865 - 627955
-            </a>
-          </li>
-          <li className="flex items-center">
-            <FaFacebook className="text-cyber-blue mr-1 md:mr-3" />
-            <a
-              href="https://www.facebook.com/profile.php?id=61557333985949"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="hover:text-white transition text-xs md:text-base"
-            >
-              Ciber ProGamers
-            </a>
-          </li>
-        </ul>
+          {/* Información de contacto */}
+          <ul className="space-y-4">
+            <li className="flex items-center">
+              <FaMapMarkerAlt className="text-cyber-blue mr-1 md:mr-3" />
+              <span className=' text-xs md:text-base'>Saavedra 278, Bº Oeste<br />Juan B. Alberdi, Tucumán</span>
+            </li>
+            <li className="flex items-center">
+              <FaEnvelope className="text-cyber-blue mr-1 md:mr-3" />
+              <span className=' text-xs md:text-base'>correotaller278@gmail.com</span>
+            </li>
+            <li className="flex items-center">
+              <FaPhone className="text-cyber-blue mr-1 md:mr-3" />
+              <a href="tel:3865627955" className="hover:text-white transition text-xs md:text-base">
+                3865 - 627955
+              </a>
+            </li>
+            <li className="flex items-center">
+              <FaFacebook className="text-cyber-blue mr-1 md:mr-3" />
+              <a
+                href="https://www.facebook.com/profile.php?id=61557333985949"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="hover:text-white transition text-xs md:text-base"
+              >
+                Ciber ProGamers
+              </a>
+            </li>
+          </ul>
+        </div>
 
-        {/* Horarios */}
-        <div className="md:mt-32 mt-24">
+        {/* HORARIOS y FORMULARIO */}
+        <div>
+          {/* Horarios */}
           <h3 className="font-bold text-gray-300 mb-2">Horarios</h3>
           <ul className="text-sm space-y-2">
             <li className=' text-xs md:text-base'>Lunes a Viernes: 09:00 - 13:00 | 17:00 - 00:00</li>
             <li className=' text-xs md:text-base'>Sábado: 08:00 - 22:00</li>
             <li className=' text-xs md:text-base'>Domingo: 11:00 - 22:00</li>
           </ul>
+          {/* Formulario de Contacto */}
+          <FormModal toggleSidebar={toggleSidebar} />
         </div>
-
-        {/* Formulario de Contacto */}
-        <FormModal toggleSidebar={toggleSidebar}/>
 
       </motion.div>
     </div>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -2,11 +2,13 @@ import { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase/firebaseConfig";
 import { useNavigate } from "react-router-dom";
+import { FaEye, FaEyeSlash } from 'react-icons/fa'
 
 export default function LoginPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const navigate = useNavigate();
 
   const handleSubmit = async (e: React.FormEvent) => {
@@ -36,13 +38,28 @@ export default function LoginPage() {
           </div>
           <div className="mb-4">
             <label className="block text-sm font-medium text-gray-700">Contraseña</label>
-            <input
-              type="password"
-              className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-cyan-500"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
+            <div className="relative flex items-center">
+              <input
+                type={showPassword ? "text" : "password"}
+                className="w-full px-3 py-2 border rounded-lg focus:outline-none focus:ring focus:ring-cyan-500 pr-10"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                type="button"
+                onClick={() => setShowPassword(!showPassword)}
+                className="absolute right-3 text-gray-500"
+                aria-label="Toggle password visibility"
+              >
+                {showPassword ? (
+                  <FaEye className="w-5 h-5 text-blue-700" />
+                ) : (
+                  <FaEyeSlash className="w-5 h-5 text-blue-700" />
+                )}
+              </button>
+            </div>
           </div>
+
           <button
             type="submit"
             className="w-full bg-cyan-500 text-white py-2 rounded-lg hover:bg-cyan-700 transition"
